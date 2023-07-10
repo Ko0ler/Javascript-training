79827cdf194aa825a7113f3062869cca68860b81
Mohamed <ouedraogomohamed008@gmail.com>
2023-07-10 15:12:17 +0000

    Update

--- Script/script.js
+++ Script/script.js
@@ -9,7 +9,7 @@ function chooseGameMode(callback) {
 
   // Function to show the available options based on the selected content type
   function showOptions(list) {
-    content.innerHTML = "" + `<option value="default">Not chosen</option>`;
+    content.innerHTML = `` + `<option value="default">Not chosen</option>`;
     for (let i = 1; i <= list.length; i++) {
       content.innerHTML += `
       <option>${list[i-1]}</option>
@@ -42,6 +42,11 @@ function chooseGameMode(callback) {
   content.addEventListener("change", event => {
     selected_mode = event.target.value;
     callback(selected_mode);
+    // Add a start button
+    let start_button = document.getElementById("start_button");
+    start_button.innerHTML = `
+    <button type="button">Start</button>
+    `;
   });
 }
 
@@ -58,12 +63,22 @@ function getSelectedMode() {
 function retrieveUserInput(type_of_item, item_number, item_value, number_of_item) {
   return new Promise((resolve, reject) => {
     let input;
-    let gameplay = document.getElementById("gameplay");
+    let instruction = document.getElementById("instruction");
     let user_input = document.getElementById("user_input");
     let input_submit = document.getElementById("input_submit");
 
     // Display the challenge to the user
-    gameplay.innerHTML = `Write the ${type_of_item} #${item_number} out of ${number_of_item} : <span class="text_to_write">${item_value}</span>`;
+    instruction.innerHTML = `Write the ${type_of_item} #${item_number} out of ${number_of_item} : <span class="text_to_write">${item_value}</span>`;
+
+    // For user pressing Enter key
+    user_input.addEventListener("keypress", event => {
+      if (event.key === "Enter") {
+        input = user_input.value;
+        resolve(input);
+      }
+    });
+
+    // For user clicking on the submit button
     input_submit.addEventListener("click", () => {
       input = user_input.value;
       resolve(input);
@@ -81,15 +96,18 @@ async function startGame() {
   let item_value = "";
   let answer = "";
   let score = 0;
+  let start = document.getElementById("start_button");
   let challenge_number = document.getElementById("challenge_number");
   let answer_review = document.getElementById("review");
-  let current_score = document.getElementById("current_score");
+  let current_info = document.getElementById("info");
   let final_score = document.getElementById("final_score");
+  let starting_time = Date.now();
+  let time_spent;
 
   // Clear previous game state
   challenge_number.textContent = "";
   answer_review.textContent = "";
-  current_score.textContent = "";
+  current_info.textContent = "";
   final_score.textContent = "";
 
   // Get the selected game mode from the user
@@ -98,40 +116,48 @@ async function startGame() {
   list_of_item = GAME_MODE[game_choice];
   number_of_item = list_of_item.length;
 
-  // Loop through each item
-  for (item_number; item_number <= number_of_item; item_number++) {
-    challenge_number.textContent = `Do the challenge ${item_number}`;
-    item_value = list_of_item[item_number - 1];
-    answer = await retrieveUserInput(type_of_item, item_number, item_value, number_of_item);
-    answer_review.removeAttribute("id", "win");
-    answer_review.removeAttribute("id", "loose");
+  start.addEventListener("click", startProcess);
 
-    // Check the user's answer and update the score
-    if (answer === item_value) {
-      answer_review.setAttribute("id", "win");
-      answer_review.textContent = "Great!";
-      score += 1;
-    } else {
-      answer_review.setAttribute("id", "loose");
-      answer_review.textContent = "Wrong!";
+  async function startProcess() {
+    // Loop through each item
+    for (item_number; item_number <= number_of_item; item_number++) {
+      challenge_number.textContent = `Do the challenge ${item_number}`;
+      item_value = list_of_item[item_number - 1];
+      answer = await retrieveUserInput(type_of_item, item_number, item_value, number_of_item);
+
+      // Check the user's answer and update the score
+      if (answer === item_value | answer === item_value + " ") {
+        answer_review.setAttribute("id", "win");
+        answer_review.textContent = "Great!";
+        score += 1;
+      } else {
+        answer_review.setAttribute("id", "loose");
+        answer_review.textContent = "Wrong!";
+      }
+
+      current_info.textContent = `Current score : ${score}`;
+
+      // Clear the user input field for the next challenge
+      let inserted_text = document.getElementById("user_input");
+      inserted_text.value = "";
+
+      // Display the time spent and the final score at the end of the game
+      if (item_number === number_of_item) {
+        time_spent = (Date.now() - starting_time)/1000;
+        current_info.innerHTML = `<u>Time spent</u> : ${time_spent} seconds`;
+        final_score.setAttribute("id", "final_score");
+        final_score.textContent = `Final score : ${score}/${number_of_item}`;
+      }
+
+      // Clear instructions
+      let instruction = document.getElementById("instruction");
+      instruction.textContent = "";
     }
 
-    current_score.textContent = `Current score : ${score}`;
-
-    // Display the final score at the end of the game
-    if (item_number === number_of_item) {
-      final_score.setAttribute("id", "final_score");
-      final_score.textContent = `Final score : ${score}/${number_of_item}`;
-    }
-
-    // Clear the user input field for the next challenge
-    let inserted_text = document.getElementById("user_input");
-    inserted_text.value = "";
+    // Hide the submit button after the game ends
+    let submit_button = document.getElementById("input_submit");
+    submit_button.innerHTML = ``;
   }
-
-  // Hide the submit button after the game ends
-  let submit_button = document.getElementById("input_submit");
-  submit_button.innerHTML = "";
 }
 
 // Function to retry the game
@@ -144,7 +170,7 @@ function retryGame() {
   let retry = document.getElementById("retry");
   retry.addEventListener("click", () => {
     // Reset the game by reloading the page
-    document.body.innerHTML = "";
+    document.body.innerHTML = ``;
     run();
   });
 }
@@ -157,7 +183,9 @@ async function run() {
     await startGame();
     retryGame();
   } catch (error) {
-    document.body.innerHTML = `Some errors occurred: ${error}`;
+    document.body.innerHTML = `
+    <h4>Some errors occurred: ${error}<h4>
+    `;
     console.log(error);
   }
 }
