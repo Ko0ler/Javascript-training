e6cbd3557369091ead55048e1fb8e80c37120b35
Mohamed <ouedraogomohamed008@gmail.com>
2023-07-14 13:48:21 +0000

    Update

--- Script/config.js
+++ Script/config.js
@@ -1,32 +1,53 @@
-// Words
-const PASSION = ["offsec", "programming", "gaming", "animes", "sport"];
-const RANDOM = ["ffuf", "wannacry", "2>/dev/null"];
-const CERTIFICATIONS = ["eJPTv2", "OSCP", "OSCE"];
+// Word Lists
+const WORD_LIST = {
+  "Fruits": ["apple", "banana", "orange", "strawberry", "kiwi"],
+  "Colors": ["red", "green", "blue", "yellow", "orange"],
+  "Passion": ["offsec", "programming", "games", "anime", "sports"],
+  "Random": ["ffuf", "wannacry", "2>/dev/null", "openai", "javascript"],
+  "Animals": ["dog", "cat", "lion", "monkey", "elephant"],
+  "Certifications": ["eJPTv2", "OSCP", "OSCE", "CEH", "CISSP"],
+  "ComplexWords": ["antidisestablishmentarianism", "sesquipedalian", "zygodactylous", "cynosure", "obfuscation"]
+};
 
-// Phrases
-const PROVERB = [
-  "Carpe diem",
-  "The only way to do great work is to love what you do",
-  "In the middle of every difficulty lies opportunity",
-  "Per aspera ad astra"
-];
-const FACTS = [
-  "Did you know that Mikael Jackson was originated from Ivory Coast?",
-  "All we see and know about the universe are million years old",
-  "Life is bullshit"
-]
+// Phrase Lists
+const PHRASE_LIST = {
+  "Facts": [
+    "Did you know that Michael Jackson was born in the Ivory Coast?",
+    "Everything we see and know about the universe is millions of years old.",
+    "Life is tough.",
+    "Elephants are the only animals that can't jump.",
+    "China is the most populous country in the world."
+  ],
+  "Proverbs": [
+    "Carpe diem",
+    "The only way to do great work is to love what you do",
+    "In the middle of every difficulty lies opportunity",
+    "Per aspera ad astra",
+    "The early bird catches the worm"
+  ],
+  "Jokes": [
+    "Why do scuba divers always dive backward and not forward? Because otherwise, they'd fall into the boat!",
+    "What's the electrician's favorite pastime? Not being current with what's going on.",
+    "Why don't fish like playing tennis? Because they're afraid of the net!"
+  ],
+  "Quotes": [
+    "The only way to do great work is to love what you do.",
+    "Life is a challenge to meet, a happiness to earn, an adventure to experience.",
+    "Success is going from failure to failure without losing your enthusiasm.",
+    "Knowledge speaks, but wisdom listens.",
+    "Don't follow your path, blaze it."
+  ],
+  "ComplexPhrases": [
+    "Peripatetic pontifications precipitate perspicacity.",
+    "Sesquipedalian verbosity often obfuscates the intended message.",
+    "Eloquence can be achieved through meticulous mastication of magniloquent vocabulary.",
+    "The ineffable profundity of the cosmos eludes the grasp of human comprehension.",
+    "The intricacies of quantum mechanics boggle the mind of even the most erudite scholar."
+  ]
+};
 
-// *******************
-
-const WORD_LIST = ["Passion", "Random", "Certs"];
-const PHRASE_LIST = ["Proverbs", "Fun facts"];
+// Game Mode Data
 const GAME_MODE = {
-  // Words
-  "Passion" : PASSION,
-  "Random" : RANDOM,
-  "Certs" : CERTIFICATIONS,
-  
-  // Phrases
-  "Proverbs" : PROVERB,
-  "Fun facts" : FACTS
+  "word" : WORD_LIST,
+  "phrase" : PHRASE_LIST
 };
--- Script/script.js
+++ Script/script.js
@@ -1,66 +1,64 @@
 let initial_page = document.body.innerHTML;
 
 // Function to let the user choose the game mode
-function chooseGameMode(callback) {
-  let choice = document.querySelectorAll('input[name="content_type"]');
-  let content = document.getElementById("content");
-  let content_list = [];
+function chooseGameMode() {
+  let type_of_item;
+  let type_of_item_choice = document.querySelectorAll('input[name="content_type"]');
+  let item_context = document.getElementById("context");
+  let list_of_context = [];
   let selected_mode;
 
   // Function to show the available options based on the selected content type
   function showOptions(list) {
-    content.innerHTML = `` + `<option value="default">Not chosen</option>`;
+    item_context.innerHTML = `` + `<option value="default">Not chosen</option>`;
     for (let i = 1; i <= list.length; i++) {
-      content.innerHTML += `
+      item_context.innerHTML += `
       <option>${list[i-1]}</option>
       `;
     }
   }
 
   // Function to handle the user's choice of content type
-  function handleUserChoice() {
-    choice.forEach(element => {
+  function getContentType() {
+    type_of_item_choice.forEach(element => {
       if (element.checked) {
         if (element.id === "word") {
-          content_list = WORD_LIST;
+          type_of_item = "word";
+          list_of_context = Object.keys(WORD_LIST);
         }
 
         if (element.id === "phrase") {
-          content_list = PHRASE_LIST;
+          type_of_item = "phrase";
+          list_of_context = Object.keys(PHRASE_LIST);
         }
       }
     });
-    showOptions(content_list);
+    showOptions(list_of_context);
   }
 
-  // Attach change event listeners to the radio buttons
-  choice.forEach(element => {
-    element.addEventListener("change", handleUserChoice);
+  // Event listener for the item type selection
+  type_of_item_choice.forEach(element => {
+    element.addEventListener("change", getContentType);
   });
 
   // Event listener for the content selection
-  content.addEventListener("change", event => {
+  item_context.addEventListener("change", event => {
     selected_mode = event.target.value;
-    callback(selected_mode);
     // Add a start button
-    let start_button = document.getElementById("start_button");
+    let start_button = document.getElementById("content_type");
     start_button.innerHTML = `
-    <button type="button">Start</button>
+    <button type="button" id="start_button">Start</button>
     `;
+  
+    let start = document.getElementById("start_button");
+    start.addEventListener("click", () => {
+      startGame(type_of_item, selected_mode);
+    });
   });
 }
 
-// Function to get the selected game mode
-function getSelectedMode() {
-  return new Promise((resolve, reject) => {
-    chooseGameMode(function(selected_mode) {
-      resolve(selected_mode);
-    })
-  });  
-}
-
 // Function to retrieve user input for a specific item
-function retrieveUserInput(type_of_item, item_number, item_value, number_of_item) {
+function retrieveUserInput(game_context, item_number, item_value, number_of_item) {
   return new Promise((resolve, reject) => {
     let input;
     let instruction = document.getElementById("instruction");
@@ -68,36 +66,38 @@ function retrieveUserInput(type_of_item, item_number, item_value, number_of_item
     let input_submit = document.getElementById("input_submit");
 
     // Display the challenge to the user
-    instruction.innerHTML = `${type_of_item} #${item_number} out of ${number_of_item} : <span class="text_to_write">${item_value}</span>`;
+    instruction.innerHTML = `${game_context} #${item_number} out of ${number_of_item} : <span class="text_to_write">${item_value}</span>`;
 
-    user_input.addEventListener("keypress", event => {
+    function getInput() {
       if (user_input.value != "") {
-        // For user pressing Enter key
-        if (event.key === "Enter") {
-          input = user_input.value;
+        input = user_input.value;
         resolve(input);
-        }
-        // For user clicking on the submit button
-        input_submit.addEventListener("click", () => {
-          input = user_input.value;
-          resolve(input);
-        });
       }
+    }
+
+    // For user pressing Enter key
+    user_input.addEventListener("keypress", event => {
+      if (event.key === "Enter") {
+        getInput();
+      }
+    });
+
+    // For user clicking on the submit button
+    input_submit.addEventListener("click", () => {
+      getInput()
     });
   });
 }
 
 // Main game function
-async function startGame() {
-  let game_choice = "";
-  let type_of_item = "";
+async function startGame(type_of_item, game_context) {
+  let type = {};
   let list_of_item = [];
   let item_number = 1;
   let number_of_item = 0;
   let item_value = "";
   let answer = "";
   let score = 0;
-  let start = document.getElementById("start_button");
   let challenge_number = document.getElementById("challenge_number");
   let answer_review = document.getElementById("review");
   let current_info = document.getElementById("info");
@@ -107,57 +107,59 @@ async function startGame() {
   let starting_time = Date.now();
   let time_spent;
 
-  // Get the selected game mode from the user
-  game_choice = await getSelectedMode();
-  type_of_item = game_choice;
-  list_of_item = GAME_MODE[game_choice];
+  let hide_choice = document.getElementById("choice_section");
+  let show_gameplay = document.getElementById("gampelay_section");
+  hide_choice.setAttribute("class", "hide_target");
+  show_gameplay.removeAttribute("class", "hide_target");
+  
+  type = GAME_MODE[type_of_item];
+  list_of_item = type[game_context];
   number_of_item = list_of_item.length;
 
-  start.addEventListener("click", startProcess);
+  function correctAnswer() {
+    answer_review.setAttribute("id", "win");
+    answer_review.textContent = "Let's go !!";
+    score += 1;
+  }
 
-  async function startProcess() {
-    // Loop through each item
-    for (item_number; item_number <= number_of_item; item_number++) {
-      challenge_number.textContent = `Do the challenge ${item_number}`;
-      item_value = list_of_item[item_number - 1];
-      answer = await retrieveUserInput(type_of_item, item_number, item_value, number_of_item);
+  function wrongAnswer() {
+    answer_review.setAttribute("id", "lose");
+    answer_review.textContent = "You suck!";
+  }
 
-      // Check the user's answer and update the score
-      if (answer === item_value | answer === item_value + " ") {
-        answer_review.setAttribute("id", "win");
-        answer_review.textContent = "Let's go !!";
-        score += 1;
-      } else {
-        answer_review.setAttribute("id", "loose");
-        answer_review.textContent = "You suck!";
-      }
+  // Loop through each item
+  for (item_number; item_number <= number_of_item; item_number++) {
+    challenge_number.textContent = `Do the challenge ${item_number}`;
+    item_value = list_of_item[item_number - 1];
+    answer = await retrieveUserInput(game_context, item_number, item_value, number_of_item);
 
-      current_info.textContent = `Current score : ${score}`;
-
-      // Clear the user input field for the next challenge
-      let inserted_text = document.getElementById("user_input");
-      inserted_text.value = "";
-
-      // Clear instructions
-      // let instruction = document.getElementById("instruction");
-      // instruction.textContent = "";
-
-      // // Hide the submit button after the game ends
-      // let submit_button = document.getElementById("input_submit");
-      // submit_button.innerHTML = ``;
+    // Reset review property
+    answer_review.removeAttribute("id");
+  
+    // Check the user's answer and update the score
+    if (answer === item_value || answer === item_value + " ") {
+      correctAnswer();
+    } else {
+      wrongAnswer();
     }
 
-    // Display the time spent and the final score at the end of the game
-    let main = document.querySelector("main");
-    main.innerHTML = ``; // Clear main content
-    time_spent = (Date.now() - starting_time)/1000;
-    final_time.innerHTML = `Time spent : <strong>${time_spent} seconds</strong>`;
-    final_score.setAttribute("id", "final_score");
-    final_score.textContent = `Final score : ${score}/${number_of_item}`;
-    final_info.setAttribute("class", "popup");
-    
-    retryGame(time_spent);
+    current_info.textContent = `Current score : ${score}`;
+
+    // Clear the user input field for the next challenge
+    let inserted_text = document.getElementById("user_input");
+    inserted_text.value = "";
   }
+
+  // Display the time spent and the final score at the end of the game
+  let main = document.querySelector("main");
+  main.innerHTML = ``; // Clear main content
+  time_spent = (Date.now() - starting_time)/1000;
+  final_time.innerHTML = `Time spent : <strong>${time_spent} seconds</strong>`;
+  final_score.setAttribute("id", "final_score");
+  final_score.textContent = `Final score : ${score}/${number_of_item}`;
+  final_info.setAttribute("class", "popup");
+  
+  retryGame(time_spent);
 }
 
 // Function to retry the game
@@ -174,16 +176,16 @@ function retryGame(time_spent) {
     // Add score to score board
     let last_score = document.getElementById("last_score");
     last_score.innerHTML = `
-    <p>Last Score : ${time_spent}
-    `
-    startGame();
+    <p>Last Score : ${time_spent}</p>
+    `;
+    run();
   });
 }
 
 // Main function to run the game
 function run() {
   try {
-    startGame();
+    chooseGameMode();
   } catch (error) {
     document.body.innerHTML = `
     <h4>Some errors occurred: ${error}<h4>
